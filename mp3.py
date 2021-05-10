bf107752899ba292989b6cd2f728a21056da8921
B3RD-MW0LU <81065844+B3RD-MW0LU@users.noreply.github.com>
2021-05-10 20:47:17 +0530

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -1,13 +1,40 @@
+
+
+
+from __future__ import unicode_literals
+
 import youtube_dl
+
 import os
 
 print("\033[1;32;40m")
+
 os.system("clear")
+
 print("\n\nWelcome to youtube audio downloader\n\n")
 
 os.system("jp2a yt.png")
+
 print("\n\n")
+
 u_url=input("Enter the youtube url : ")
-ydl_opts = {}
+
+ydl_opts = {
+
+    'format': 'bestaudio/best',
+
+    'postprocessors': [{
+
+        'key': 'FFmpegExtractAudio',
+
+        'preferredcodec': 'mp3',
+
+        'preferredquality': '192',
+
+    }],
+
+}
+
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+
     ydl.download([u_url])
